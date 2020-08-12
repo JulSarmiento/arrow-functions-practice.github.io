de8281a064225bfc0a50dbc151b92b1c18d9ecbb
JulSarmiento <jasa1999@hotmail.com>
2020-08-12 15:06:30 -0500

    termine el HTML, tengo pendiente parte del CSS y todo el JS

---
+++ scipts/script.js
@@ -0,0 +1,5 @@
+// Necesito una funcion que reciba los numeros
+// NEcesito una funcion que almaece los numeros y los guarde en el localstorage 
+// una funcion que imprima el array 
+// una funcion que sume los numeros y les del promedio
+// Una funcion que imprima el resultado del promedio
